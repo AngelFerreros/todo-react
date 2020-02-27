71ec46932f85baafb9c1fd8eb9ea9fd4beded530
Angel Ferreros <AngelFerreros@TJs-MacBook-Pro.local>
2020-02-27 20:08:27 +0800

    Can append list item

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -22,29 +22,30 @@ class App extends React.Component {
     // a button click handler that takes what is in the input, removes it from the input and pushes it into the list.
     // write the code that renders the list
     debugger
-    let task = this.state.word;
-    console.log('pushing task into array', task)
+    let currentTask = this.state.word;
+    console.log('pushing task into array', currentTask)
     let taskArr = this.state.list;
-    taskArr.push(task);
+    taskArr.push(currentTask);
     this.setState({word:" "});
     console.log(taskArr);
+    let listItem = document.createElement('li');
+    listItem.innerHTML = currentTask;
+    document.getElementById('taskList').appendChild(listItem);
   }
-
-
   render() {
-    // let tasks = this.state.list.map (task => <li> {task} </li>)
-
+    console.log('rendering');
     return (
       <div>
         <h1> Add Task: </h1>
-          <input type ="text" id ="taskName" placeholder="Add task" onChange={()=>{this.getInputValue(event);}} value = {this.state.word.toUpperCase()}/>
+          <input type ="text" id ="taskName" placeholder="Add task" onChange={()=>{this.getInputValue(event);}} value = {this.state.word}/>
 
           <div className = "submit-btn">
             <button type ="button" onClick={()=>{this.handleClick(event);}}>Submit</button>
           </div>
 
           <div className = "tasks">
-            <ul>
+          <h2> My Tasks:</h2>
+            <ul id = "taskList">
             </ul>
           </div>
       </div>
