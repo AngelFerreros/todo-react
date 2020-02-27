f9084497b34c627c25783b22c1d8d4fa891249ed
Angel Ferreros <AngelFerreros@TJs-MacBook-Pro.local>
2020-02-27 16:39:54 +0800

    Initial commit

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
+
 class App extends React.Component {
+  constructor(){
+    super()
+    console.log("input value");
+    this.state = {
+      word : "",
+      error: false
+    }
+  }
+
+  getInputValue(event){
+    this.setState({word:event.target.value});
+    console.log('task:',event.target.value)
+  }
+
   render() {
     return (
       <div>
-        Welcome.
+        <h1> Add Task: </h1>
+          <input type ="text" id ="taskName" placeholder="Add task" onChange={()=>{this.getInputValue(event);}} value = {this.state.word.toUpperCase()}/>
+
+          <div className = "submit-btn">
+            <button type ="button" onClick={()=>{this.handleClick(event);}}>Submit</button>
+          </div>
       </div>
     );
   }
