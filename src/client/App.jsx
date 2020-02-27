bae4d3571b13a6dd612bf19fec77a8d66dbe463c
Angel Ferreros <AngelFerreros@TJs-MacBook-Pro.local>
2020-02-27 16:49:56 +0800

    Able to handle click event - get ahold of value and clear input field

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -17,7 +17,20 @@ class App extends React.Component {
     console.log('task:',event.target.value)
   }
 
+  handleClick(event){
+    // a button click handler that takes what is in the input, removes it from the input and pushes it into the list.
+    // write the code that renders the list
+    // debugger
+    let task = this.state.word;
+    console.log('pushing task into array', task)
+    this.setState({word:" "});
+  }
+
+
   render() {
+    let taskList = [];
+    let tasks = taskList.map (task => <li> {task} </li>)
+
     return (
       <div>
         <h1> Add Task: </h1>
@@ -25,6 +38,11 @@ class App extends React.Component {
 
           <div className = "submit-btn">
             <button type ="button" onClick={()=>{this.handleClick(event);}}>Submit</button>
+          </div>
+
+          <div className = "tasks">
+            <ul>
+            </ul>
           </div>
       </div>
     );
